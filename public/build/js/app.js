279ccd6de7118c212d3d8341c8c8947a0f27ca86
Sebastian BLR <blademirlabra@gmail.com>
2022-08-31 15:11:40 -0500

    Primeros pasos con la Navegación por Tabs

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,1 +1,1 @@
-alert("Hola");
+let paso=1;function iniciarApp(){tabs()}function mostrarSeccion(){}function tabs(){document.querySelectorAll(".tabs button").forEach(t=>{t.addEventListener("click",(function(t){paso=parseInt(t.explicitOriginalTarget.dataset.paso),mostrarSeccion()}))})}document.addEventListener("DOMContentLoaded",(function(){iniciarApp()}));
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,22 @@
-alert('Hola');
+let paso = 1;
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp(){
+    tabs(); //Cambia la Seccion cuando se precionen los tabs
+}
+
+function mostrarSeccion(){
+    
+}
+function tabs(){
+    const botones = document.querySelectorAll('.tabs button');
+    botones.forEach(boton => {
+        boton.addEventListener('click', function(e){
+            paso = parseInt(e.explicitOriginalTarget.dataset.paso);
+            mostrarSeccion();
+        });
+    });
+}
+
