dd7f90a33fb4f35770f04d877c6a5969749ef5ca
Sebastian BLR <blademirlabra@gmail.com>
2022-08-31 12:06:12 -0500

    Añadiendo los Contenedores Principales

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,19 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuación</p>
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p>Elige tus servicios a continuacion</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus Datos y Cita</h2>
+        <p>Coloca tus datos y fecha para tu cita</p>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verifica que la informacion sea correcta</p>
+        
+    </div>
+</div>
