a17b2430832f5271c50f1238a33d974ed1a4d19d
Sebastian BLR <blademirlabra@gmail.com>
2022-08-31 15:36:25 -0500

    Finalizando la navegación por tabs

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function iniciarApp(){
+    mostrarSeccion();
     tabs(); //Cambia la Seccion cuando se precionen los tabs
+    
 }
 
 function mostrarSeccion(){
@@ -17,6 +19,14 @@ function mostrarSeccion(){
     const seccion = document.querySelector(`#paso-${paso}`);
     seccion.classList.add('mostrar');
     
+    // Quitar la clase actual al anterior :D
+    const tabAnterior = document.querySelector('.actual');
+    if(tabAnterior)
+    tabAnterior.classList.remove('actual');
+    // resalta el tab actual
+    const tab = document.querySelector(`[data-paso = "${paso}"]`);
+    tab.classList.add('actual');
+
 }
 function tabs(){
     const botones = document.querySelectorAll('.tabs button');
