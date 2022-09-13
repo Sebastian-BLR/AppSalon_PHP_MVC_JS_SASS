2da7f7210a0c5a6420f1dd36144185b17793080f
Sebastian BLR <blademirlabra@gmail.com>
2022-09-13 10:28:02 -0500

    542. Proteger el Panel de administración

--- includes/funciones.php
+++ includes/funciones.php
@@ -21,6 +21,12 @@ function isAuth() : void {
         header('Location: /');
 }
 
+// Funcion que revisa si el usuario es admin
+function isAdmin() : void {
+    if(!isset($_SESSION['admin']))
+        header('Location: /');
+}
+
 function esUltimo(string $actual,string $proximo) :bool{
     return $actual !== $proximo;
         
