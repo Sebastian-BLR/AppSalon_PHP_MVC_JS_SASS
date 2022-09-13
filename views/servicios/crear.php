1d0599dfadbef2bc6bf0831d7acf4f36bd71b581
Sebastian BLR <blademirlabra@gmail.com>
2022-09-13 12:55:23 -0500

    546. Creando el Formulario para Servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -2,3 +2,8 @@
 <p class="descripcion-pagina">Llena todos los campos para crear un servicio</p>
 
 <?php include_once __DIR__ . '/../templates/barra.php';?>
+
+<form action="/servicios/crear" method="post" class="formulario">
+    <?php include_once __DIR__ . '/formulario.php';?>
+    <input type="submit" value="Guardar Servicio" class="boton r-boton">
+</form>
